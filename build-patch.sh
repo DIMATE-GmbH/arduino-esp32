530c4d994aedcbfb82f96f6f0c6491e20ff2ee87
Tobias Hahnen <hahnen@dimate.de>
2026-04-01 14:05:47 +0200

    NO-JIRA: Create ZIP archive with sub-folder
    Otherwise this would not be accepted by Sloeber.

--- build-patch.sh
+++ build-patch.sh
@@ -5,24 +5,33 @@
 # (the Eclipse-based Arduino IDE).
 
 VERSION="3.3.7.2"
-ARCHIVE="esp32-core-$VERSION.zip"
+FOLDER="esp32-core-$VERSION"
+ARCHIVE="$FOLDER.zip"
 
+rm -rf $FOLDER
 rm -f $ARCHIVE
-zip -r $ARCHIVE \
-  cores/ \
-  libraries/BluetoothSerial/ \
-  libraries/FS/ \
-  libraries/SD/ \
-  libraries/SD_MMC/ \
-  libraries/SPI/ \
-  tools/ \
-  variants/pico32/ \
-  Kconfig.projbuild \
-  package.json \
-  boards.txt \
-  CMakeLists.txt \
-  platform.txt \
-  programmers.txt \
-  idf_component.yml
+
+mkdir $FOLDER
+mkdir $FOLDER/libraries
+mkdir $FOLDER/variants
+
+cp -a cores $FOLDER
+cp -a libraries/BluetoothSerial $FOLDER/libraries
+cp -a libraries/FS $FOLDER/libraries
+cp -a libraries/SD $FOLDER/libraries
+cp -a libraries/SD_MMC $FOLDER/libraries
+cp -a libraries/SPI $FOLDER/libraries
+cp -a tools $FOLDER
+cp -a variants/pico32 $FOLDER/variants
+cp Kconfig.projbuild $FOLDER
+cp package.json $FOLDER
+cp boards.txt $FOLDER
+cp CMakeLists.txt $FOLDER
+cp platform.txt $FOLDER
+cp programmers.txt $FOLDER
+cp idf_component.yml $FOLDER
+
+zip -r $ARCHIVE $FOLDER >/dev/null
 
 sha256 $ARCHIVE
+stat -f%z $ARCHIVE
